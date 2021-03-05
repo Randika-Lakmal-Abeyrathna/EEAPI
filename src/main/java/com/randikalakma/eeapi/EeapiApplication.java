ef14a300bb084e90e881d78fce999a189dcdc1a8
Randika Lakmal <randika.help@gmail.com>
2021-03-05 22:12:48 +0530

    Added Async For Sending email
    Enabled the Async and Applied to sending email.

--- src/main/java/com/randikalakma/eeapi/EeapiApplication.java
+++ src/main/java/com/randikalakma/eeapi/EeapiApplication.java
@@ -2,8 +2,10 @@ package com.randikalakma.eeapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class EeapiApplication {
 
 	public static void main(String[] args) {
